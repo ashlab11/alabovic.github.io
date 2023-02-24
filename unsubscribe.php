59761f8766f3a72e6de47fe494853fb9ea1d8e2b
ashlab11 <asherlabovich@gmail.com>
2023-02-23 21:25:24 -0500

    created files

---
+++ unsubscribe.php
@@ -0,0 +1,10 @@
+<?php
+// Get the user's email address from the form submission
+$email = $_POST['email'];
+
+// TODO: Implement code to remove the email address from your mailing list
+
+// Redirect the user to a confirmation page
+header('Location: confirmation.html');
+exit;
+?>
